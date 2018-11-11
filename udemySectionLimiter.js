3f5a38083b9f37d05fec978d7aaffe8a169dd728
Charles Pustejovsky <cpustejovsky@bitpay.com>
2018-11-11 17:15:25 -0500

    small changes to udemySelectionDurations and added udemySectionLimiter to be able to see what section one could get to in x minutes.

--- udemySectionLimiter.js
+++ udemySectionLimiter.js
@@ -1,16 +1,13 @@
-/*
-This line of code is meant to be put into the console.
-DO NOT PUT INTO CONSOLE UNLESS YOU EITHER TRUST WHERE YOU GOT THIS CODE OR UNDERSTAND JAVASCRIPT
-*/
-
-function udemySectionDurations(startingSection, endingSection) {
+function udemySectionLimiter (startingSection, limiterInMinutes) {
     // Inspect the duration element on your browser to find the updated class name
     var durations = document.documentElement.getElementsByClassName('curriculum-item--duration--1OEOp');
     var count = 0;
-    for (var i = (startingSection-1); i < (endingSection); i++) {
+    var limiter = 60*(parseFloat(limiterInMinutes));
+    for (var i = (startingSection-1); count <= limiter; i++) {
         durationAsString = durations[i].innerHTML;
         // This logs all of the durations to help you make sure you picked the correct start and end points
         console.log(durationAsString);
+        console.log(i);
         if (durationAsString === "") {
             continue;
         } else {
@@ -24,12 +21,10 @@ function udemySectionDurations(startingSection, endingSection) {
             tens (seconds): [3]
             ones (seconds): [4]
             */
-           durationAsString.split(':');
-           var seconds = parseFloat(durationAsString[0]) * 600 + parseFloat(durationAsString[1]) * 60 + parseFloat(durationAsString[3]) * 10 + parseFloat(durationAsString[4]);
-           count += seconds;
+            durationAsString.split(':');
+            var seconds = parseFloat(durationAsString[0]) * 600 + parseFloat(durationAsString[1]) * 60 + parseFloat(durationAsString[3]) * 10 + parseFloat(durationAsString[4]);
+            count += seconds;
         }
-    };
-    console.log("The selected lectures are " + count + " seconds long.");
-    console.log("The selected lectures are " + (count/60) + " minutes long.");
-    console.log("The selected lectures are " + (count/3600) + " hours long.");
+    }
+    console.log(`The selected lectures are ${count/60} minutes long and should stop at lecture ${i}.`);
 }
